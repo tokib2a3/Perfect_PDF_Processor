5ebeddb749b9ec87806695fc7bc4f53e3da025f5
tokib2a3 <147452699+tokib2a3@users.noreply.github.com>
2024-01-14 19:14:31 +0900

    merge_into_single_page: ページのマージ方法を変更

--- merge_into_single_page.py
+++ merge_into_single_page.py
@@ -19,25 +19,26 @@ for inputFile in sys.argv[1:]:
   page_width = new_page.mediabox.width / yokopages
   page_height = new_page.mediabox.height / tatepages
 
-  # Loop through each cell of the 3x4 grid
-  for i in range(0, tatepages):
-    for j in range(0, yokopages):
+  # Loop through each cell of the ixj grid
+  for i in range(tatepages):
+    for j in range(yokopages):
       page_index = i * yokopages + j
 
       # Check if there are more pages to add
       if page_index < len(reader.pages):
         page = reader.pages[page_index]
         page.scale_to(page_width, page_height)
-        page.mediabox = new_page.mediabox
-        page.cropbox = new_page.cropbox
 
         # Calculate the coordinates of the cell
         x = j * page_width
         y = (tatepages - i - 1) * page_height  # Reverse the order for correct placement
 
         # Add the page to the cell
-        page.add_transformation(Transformation().translate(x, y))
-        new_page.merge_page(page)
+        new_page.merge_transformed_page(
+          page,
+          Transformation().translate(x, y),
+        )
+
 
   # Save the modified PDF
   outputFileName = os.path.splitext(inputFile)[0] + "_merged.pdf"
